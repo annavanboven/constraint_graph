74a2c4bd3fe3f7fe8434aac087126acfba2b74d6
Anna Van Boven <anna.van.boven@gmail.com>
2025-12-05 08:32:31 -0700

    agent sweep

--- run_scripts/create_sweep.py
+++ run_scripts/create_sweep.py
@@ -1,6 +1,9 @@
 import wandb 
 from practice_gnn_network import setup_wandb
 
+
 sweep_config = setup_wandb()
 sweep_id = wandb.sweep(sweep_config, project="constraint_graph_test")
-print(sweep_id)
+
+with open("sweep_id.txt", "w") as f:
+    f.write(sweep_id)
