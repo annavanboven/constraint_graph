b1aee2b6797d732889b455d77421f3cf1a850e20
annavanboven <anna.van.boven@gmail.com>
2026-01-02 12:28:34 -0700

    alpine working but gnn is not

--- run_scripts/create_opf_sweep.py
+++ run_scripts/create_opf_sweep.py
@@ -1,9 +1,10 @@
 import wandb 
-from practice_gnn_network import setup_wandb
+from train_pdgnn import setup_wandb
 
 
 sweep_config = setup_wandb()
 sweep_id = wandb.sweep(sweep_config, project="case6ww_acopf")
 
-with open("sweep_id.txt", "w") as f:
-    f.write(sweep_id)
+# with open("sweep_id.txt", "w") as f:
+#    f.write(sweep_id)
+print(sweep_id)
